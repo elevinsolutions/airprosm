d659f872a52af2ae1a2f8ad76cb45ea080fb463f
enzo <jlcruz@elevinsolution.com>
2018-02-26 12:49:51 -0700

    minor navbar cosmetic change

--- themes/airprosm/header.php
+++ themes/airprosm/header.php
@@ -32,6 +32,7 @@
         .navbar-inverse {
             background-color: #a52424f2;
             border-color: transparent;
+            box-shadow: 0 0 1px 1px rgba(20,23,28,.1), 0 3px 1px 0 rgba(20,23,28,.1);
         }
         .navbar-inverse .navbar-nav>li>a {
             color: #ececec;
